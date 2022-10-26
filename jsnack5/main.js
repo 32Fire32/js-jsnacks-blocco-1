b4918505f68283659b92e7b16149b23031638f7f
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-26 12:37:02 +0200

    end

---
+++ jsnack5/main.js
@@ -0,0 +1,20 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+const numbers = [];
+let number;
+
+for ( let i = 0; i < 6; i++ ) {
+    number = Number(prompt("inserisci un numero"));
+       
+    if ( number % 2 != 0 ) {
+        numbers.push(number);
+    }
+}
+
+
+
+
+
+console.log(numbers);
