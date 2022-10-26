6da7061667cf4ddafcf6cc01b5c6177151c867a0
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-26 10:51:25 +0200

    end

---
+++ jsnack2/main.js
@@ -0,0 +1,8 @@
+const word1 = prompt("prima parola");
+const word2 = prompt("seconda parola");
+
+if ( word1.lenght > word2.lenght) {
+    alert("la prima parola è più lunga");
+} else {
+    alert("la seconda è più lunga");
+}
