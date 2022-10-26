109bd676febff0e770f4fc22d760f1e1471d5b11
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-26 10:52:47 +0200

    correzione

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,7 +1,7 @@
 const word1 = prompt("prima parola");
 const word2 = prompt("seconda parola");
 
-if ( word1.lenght > word2.lenght) {
+if ( word1.length > word2.length) {
     alert("la prima parola è più lunga");
 } else {
     alert("la seconda è più lunga");
